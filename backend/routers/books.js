3431c5caeea86d0477b486108af5ad74714a86d8
zee <fzr72725@gmail.com>
2024-01-24 15:01:48 -0800

    working version of button calling books component

--- backend/routers/books.js
+++ backend/routers/books.js
@@ -4,7 +4,7 @@ import * as booksJson from '../resources/books.json' assert { type: "json" };
 const router = express.Router();
 
 router.get('/api/v1/books', (req, res) => {
-  console.log('ZZZZZ get all books');
+  //console.log('ZZZZZ get all books');
   res.json(booksJson);
 })
 
