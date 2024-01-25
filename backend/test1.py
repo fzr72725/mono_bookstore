a851d460d71def3a5a52c613a8a9eed8d0bbbceb
zee <fzr72725@gmail.com>
2024-01-24 16:20:29 -0800

    print df as html works

--- backend/test1.py
+++ backend/test1.py
@@ -1,5 +1,7 @@
 import sys
 import pandas as pd
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', None)
 import json
 from pandas import json_normalize
 
@@ -9,12 +11,12 @@ def json_to_df(file_name):
     length = len(list_raw)
     df_raw = json_normalize(list_raw)
     return df_raw
-print("python script is running now...")
+# print("python script is running now...")
 d = {'col1': [1, 2], 'col2': [3, 4]}
 df = json_to_df('resources/paveSchema.json')
 #df = pd.DataFrame(data=d)
 
 # dataToSendBack = "This is from Python"
 # print(dataToSendBack)
-print(df)
+print(df.head(1).to_html())
 sys.stdout.flush()
